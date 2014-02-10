b77ceac38b4e995cff9f295e1e1b98570ee6e326
Ben Laplanche <ben@laplanche.co.uk>
2014-02-10 20:42:08 +0000

    updated

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ActiveRecord::Base
-  belongs_to :sender, class_name: 'User'
-  belongs_to :receiver, class_name: 'User'
+  has_and_belongs_to_many :users
+  # belongs_to :sender, class_name: 'User'
+  # belongs_to :receiver, class_name: 'User'
 
   validates :body, presence: true
   validates :receiver_id, presence: true
