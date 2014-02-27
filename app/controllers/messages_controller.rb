d428c4e78ff7c6c17eca5ee30b37105c30c04ecb
Ben Laplanche <ben@laplanche.co.uk>
2014-02-27 22:53:44 +0000

    added use of struct to combine objects for feed

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -39,51 +39,13 @@ class MessagesController < ApplicationController
   end
   helper_method :message
 
-def messages
-  sent_stream = current_user.sent_messages.order(:created_at).to_enum :find_each
-  received_stream = current_user.received_messages.order(:created_at).to_enum :find_each
-  ActivityAggregator.new([sent_stream, received_stream]).next_activities(1000)
-end
+  def messages
+  # TODO pass in the limit value & use will_paginate gem
+    sent_stream = current_user.sent_messages.order(:created_at).to_enum :find_each
+    received_stream = current_user.received_messages.order(:created_at).to_enum :find_each
 
-  # def messages
-  #   # build our feed, showing sent & received messages
-  #   # where the user has sent & received the message, only show against them as a sender
-  #   # Improvement 1: Use eager loading on the objects when getting the receivers & sender?
-  #   # Improvement 2: any way of stripping out the received messages from the sender earlier?
-  #   # Improvement 3: reduce the amount of "response_hash" lines to make it tidier
-  #     sent_messages = current_user.sent_messages
-  #     received_messages = current_user.received_messages
-
-  #     puts sent_messages.to_yaml
-  #     puts received_messages.to_yaml
-
-  #     response_hash = Hash.new { |hash, key| hash[key] = Hash.new(&hash.default_proc)}
-
-  #     sent_messages.each_with_index do |n,i|
-  #       k = "message" + i.to_s
-  #       response_hash[k]["message_id"] = n.id
-  #       response_hash[k]["sender"] = n.sender.email
-  #       response_hash[k]["receivers"] = n.receivers.map(&:email).join(', ')
-  #       response_hash[k]["body"] = n.body
-  #       response_hash[k]["sent"] = n.created_at
-  #       response_hash[k]["status"] = n.read
-  #     end
-
-  #     received_messages.each_with_index do |n, i|
-  #       if n.sender != current_user
-  #         k = "message" + (i +10).to_s
-  #         response_hash[k]["message_id"] = n.id
-  #         response_hash[k]["sender"] = n.sender.email
-  #         response_hash[k]["receivers"] = n.receivers.map(&:email).join(', ')
-  #         response_hash[k]["body"] = n.body
-  #         response_hash[k]["sent"] = n.created_at
-  #         response_hash[k]["status"] = n.read
-  #       end
-  #     end
-
-  #     puts response_hash.to_yaml
-  #     return response_hash
-  # end
+    ActivityAggregator.new(current_user.id, [sent_stream, received_stream]).next_activities(1000)
+  end
   helper_method :messages
 
   def receivers
--- app/models/concerns/activity_aggregator.rb
+++ app/models/concerns/activity_aggregator.rb
@@ -1,7 +1,11 @@
 class ActivityAggregator
-# from http://blog.givegab.com/post/75043413459/using-enumerations-to-make-a-faster-activity-feed-in
-  def initialize(streams)
-    @streams = streams
+# based upon http://blog.givegab.com/post/75043413459/using-enumerations-to-make-a-faster-activity-feed-in
+# TODO pass in the received_messages stream senders, so we can eager load with find_each, instead of individual queries below
+# TODO add a unique check on messages (e.g. sender & receiver are not the same person)
+# TODO return the receiver id's & message read status nested so can be hyperlinked and coloured
+  def initialize(current_user, streams)
+    @streams = streams.flatten
+    @userid = current_user
   end
 
   def next_activities(limit)
@@ -14,6 +18,21 @@ class ActivityAggregator
 
 private
   def next_activity
+    msg = select_next_activity
+    response = Struct.new(:id, :sender, :receivers, :created_at, :read, :body, :sender_id)
+    output = response.new(msg.id,
+                  msg.sender.email,
+                  msg.receivers.map(&:email).join(', '),
+                  msg.created_at,
+                  msg.read,
+                  msg.body,
+                  msg.sender.id)
+    return output
+  end
+
+  def select_next_activity
+    # subtle bug with the sorting, due to the create action the microseconds on the inserts for multiple
+    # receivers means they are sorted incorrectly, removing duplicate message ids should safely ignore this
     @streams.select{ |s| has_next?(s) }.
       sort_by{ |s| s.peek.created_at }.
       last.next
